33cd61dae4a293aca05b1745b9cda84d7c8fe575
Jun Ito <jun_ito@voyagegroup.com>
2017-01-26 18:57:46 +0900

    Updates

--- build.js
+++ build.js
@@ -36,16 +36,13 @@ AFRAME.registerComponent('crawling-cursor', {
             var intersection = getNearestIntersection(e.detail.intersections);
             if (!intersection) {return;}
 
-            var global_normal = intersection.face.normal.clone();
-
             // a matrix which represents item's movement, rotation and scale on global world
             var mat = intersection.object.matrixWorld;
-
             // remove parallel movement from the matrix
             mat.setPosition(new THREE.Vector3(0, 0, 0));
 
             // change local normal into global normal
-            global_normal.applyMatrix4(mat).normalize();
+            var global_normal = intersection.face.normal.clone().applyMatrix4(mat).normalize();
 
             // look at target coordinate = intersection coordinate + global normal vector
             var lookAtTarget = new THREE.Vector3().addVectors(intersection.point, global_normal);
