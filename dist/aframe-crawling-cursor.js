20f1fc35a0432509d1d48476e807c138ff87db31
Michael Saunders <michael@Michaels-Air.fios-router.home>
2018-08-01 15:19:22 -0400

    fixed for aframe 0.8.0 by changing event listener to tick function

--- dist/aframe-crawling-cursor.js
+++ dist/aframe-crawling-cursor.js
@@ -53,73 +53,70 @@
 	/**
 	 * Crawling Cursor component for A-Frame.
 	 */
-	AFRAME.registerComponent('crawling-cursor', {
-	    dependencies: ['raycaster'],
-	    schema: {
-	        target: {
-	            type: "selector"
-	        },
-	        offset: {
-	            // How far above the intersection point does the cursor hover? (Default 5cm)
-	            type: "number",
-	            default: 0.05,
-	        }
-	    },
+	 AFRAME.registerComponent('crawling-cursor', {
+ 	    dependencies: ['raycaster'],
+ 	    schema: {
+ 	        target: {
+ 	            type: "selector"
+ 	        },
+ 	        offset: {
+ 	            // How far above the intersection point does the cursor hover? (Default 5cm)
+ 	            type: "number",
+ 	            default: 0.05,
+ 	        }
+ 	    },
 
-	    multiple: false,
+ 	    multiple: false,
 
-	    init: function() {
-	        var el = this.el;
-	        var data = this.data;
+ 	    init: function() {
+ 	        var el = this.el;
+ 	        var data = this.data;
 
-	        if (data.target === null) {
-	            var cursor = document.querySelector("a-cursor");
+ 	        if (data.target === null) {
+ 	            var cursor = document.querySelector("a-cursor");
 
-	            if (cursor === null) {
-	                console.warn("Please put a-cursor in a document");
-	                return;
-	            }
+ 	            if (cursor === null) {
+ 	                console.warn("Please put a-cursor in a document");
+ 	                return;
+ 	            }
 
-	            data.target = cursor;
-	        }
+ 	            data.target = cursor;
+ 	        }
 
-	        el.addEventListener("raycaster-intersection", function(e) {
+ 	    },
+ 			tick: function() {
+         console.log(this.data);
+ 				var intersection = getNearestIntersection(this.el.components.raycaster.intersections,this.data);
+ 				if (!intersection) { return; }
 
-	            var intersection = getNearestIntersection(e.detail.intersections);
-	            if (!intersection) { return; }
+ 				// a matrix which represents item's movement, rotation and scale on global world
+ 				var mat = intersection.object.matrixWorld;
+ 				// remove parallel movement from the matrix
+ 				mat.setPosition(new THREE.Vector3(0, 0, 0));
 
-	            // a matrix which represents item's movement, rotation and scale on global world
-	            var mat = intersection.object.matrixWorld;
-	            // remove parallel movement from the matrix
-	            mat.setPosition(new THREE.Vector3(0, 0, 0));
+ 				// change local normal into global normal
+ 				var global_normal = intersection.face.normal.clone().applyMatrix4(mat).normalize();
 
-	            // change local normal into global normal
-	            var global_normal = intersection.face.normal.clone().applyMatrix4(mat).normalize();
+ 				// look at target coordinate = intersection coordinate + global normal vector
+ 				var lookAtTarget = new THREE.Vector3().addVectors(intersection.point, global_normal);
+ 				this.data.target.object3D.lookAt(lookAtTarget);
 
-	            // look at target coordinate = intersection coordinate + global normal vector
-	            var lookAtTarget = new THREE.Vector3().addVectors(intersection.point, global_normal);
-	            data.target.object3D.lookAt(lookAtTarget);
+ 				// cursor coordinate = intersection coordinate + normal vector * offset
+ 				var cursorPosition = new THREE.Vector3().addVectors(intersection.point, global_normal.multiplyScalar(this.data.offset));
+ 				this.data.target.setAttribute("position", cursorPosition);
 
-	            // cursor coordinate = intersection coordinate + normal vector * offset
-	            var cursorPosition = new THREE.Vector3().addVectors(intersection.point, global_normal.multiplyScalar(data.offset));
-	            data.target.setAttribute("position", cursorPosition);
+ 				function getNearestIntersection(intersections, data) {
+ 						for (var i = 0, l = intersections.length; i < l; i++) {
 
-	            function getNearestIntersection(intersections) {
-	                for (var i = 0, l = intersections.length; i < l; i++) {
+ 								// ignore cursor itself to avoid flicker && ignore "ignore-ray" class
+ 								if (data.target === intersections[i].object.el || intersections[i].object.el.classList.contains("ignore-ray")) { continue; }
+ 								return intersections[i];
+ 						}
+ 						return null;
+ 				}
 
-	                    // ignore cursor itself to avoid flicker && ignore "ignore-ray" class
-	                    if (data.target === intersections[i].object.el || intersections[i].object.el.classList.contains("ignore-ray")) { continue; }
-	                    return intersections[i];
-	                }
-	                return null;
-	            }
-	        });
-
-	        setInterval(function() {
-	            el.components.raycaster.refreshObjects();
-	        }, 100)
-	    }
-	});
+       }
+ 	});
 
 /***/ })
 /******/ ]);
